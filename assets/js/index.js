84f5f3f8ce95c6d6025a105ab0ea9cf5b99e6d99
patri¨ <patri@local>
2021-11-11 16:09:33 +0100

    third commit

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,3 +5,13 @@ const game = new Game(canvas)
 
 game.start()
 
+const player = new Player(ctx);
+
+
+setInterval(() => {
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    player.draw();
+    player.move();
+    
+}, 1000 / 60);
+
--- assets/js/player.js
+++ assets/js/player.js
@@ -2,17 +2,33 @@ class Player {
     constructor(ctx) {
         this.ctx= ctx;
         ctx.beginPath(ctx);
-ctx.arc(100, 75, 50, 0, 2 * Math.PI);
-ctx.stroke();
-        this.x=  100;
-        this.y = 100;
-        this.w = 25;
-        this.h = 25;
+    ctx.arc(150, 75, 10, 0, 2 * Math.PI);
+    ctx.stroke();
+        this.x=  150;
+        this.y = 75;
+        this.w = 10;
+        this.h = 0;
+        this.vx = 1;
+        this.vy = 2;
     }
 
     draw() {
         this.ctx.fillRect(this.x, this.y, this.w, this.h);
-        
+       
+    }
 
+    move() {
+        this.x += this.vx;
+        if (this.x + this.w >= this.ctx.canvas.width) {
+            this.vx = -this.vx;
+        }
+
+        if (this.x <= 0) {
+            this.vx  -this.vx;
+        }
+
+        if (this.y + this.h >= this.ctx.canvas.height) {
+            this.vy = -this.vy;
+        }
     }
 }
